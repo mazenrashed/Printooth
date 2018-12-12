1b1e3b27371cb920c1d2d6a5ed4db873a41fb290
Mazen Rashed <mrmazenrashed@gmail.com>
2018-12-12 12:41:38 +0200

    try to print images in woosim printers

--- printooth/src/main/java/com/mazenrashed/printooth/utilities/Printing.kt
+++ printooth/src/main/java/com/mazenrashed/printooth/utilities/Printing.kt
@@ -2,9 +2,12 @@ package com.mazenrashed.printooth.utilities
 
 import android.bluetooth.BluetoothDevice
 import android.content.Context
+import android.graphics.Bitmap
 import android.os.Handler
 import android.os.Looper
 import com.mazenrashed.printooth.data.*
+import android.R.array
+import java.nio.ByteBuffer
 
 
 class Printing(private var printer: Printer,private var pairedPrinter: PairedPrinter, context: Context) {
@@ -45,15 +48,20 @@ class Printing(private var printer: Printer,private var pairedPrinter: PairedPri
         //init printer
         bluetooth.send(printer.initPrinterCommand)
         this.printables.forEach {
-            bluetooth.send(printer.justificationCommand.plus(it.alignment))
-            bluetooth.send(printer.fontSizeCommand.plus(it.fontSize))
-            bluetooth.send(printer.emphasizedModeCommand.plus(it.bold))
-            bluetooth.send(printer.underlineModeCommand.plus(it.underlined))
-            bluetooth.send(printer.characterCodeCommand.plus(it.characterCode))
-            bluetooth.send(printer.lineSpacingCommand.plus(it.lineSpacing))
-            bluetooth.send(StringUtils.getStringAsByteArray(it.text))
+            if (it.image != null){
+                bluetooth.send(getBitmapBytes(it.image))
+            }else{
+                bluetooth.send(printer.justificationCommand.plus(it.alignment))
+                bluetooth.send(printer.fontSizeCommand.plus(it.fontSize))
+                bluetooth.send(printer.emphasizedModeCommand.plus(it.bold))
+                bluetooth.send(printer.underlineModeCommand.plus(it.underlined))
+                bluetooth.send(printer.characterCodeCommand.plus(it.characterCode))
+                bluetooth.send(printer.lineSpacingCommand.plus(it.lineSpacing))
+                bluetooth.send(StringUtils.getStringAsByteArray(it.text))
+            }
             if (it.newLinesAfter > 0)
                 bluetooth.send(printer.feedLineCommand.plus(it.newLinesAfter.toByte()))
+
         }
         //Feed 2 lines to cut the paper
         bluetooth.send(printer.feedLineCommand.plus(2))
@@ -63,10 +71,52 @@ class Printing(private var printer: Printer,private var pairedPrinter: PairedPri
         }, 2000)
     }
 
+
+    private fun getBitmapBytes(bitmap: Bitmap): ByteArray {
+        val chunkNumbers = 10
+        val bitmapSize = bitmap.rowBytes * bitmap.height
+        val imageBytes = ByteArray(bitmapSize)
+        val rows: Int
+        val cols: Int
+        val chunkHeight: Int
+        val chunkWidth: Int
+        cols = Math.sqrt(chunkNumbers.toDouble()).toInt()
+        rows = cols
+        chunkHeight = bitmap.height / rows
+        chunkWidth = bitmap.width / cols
+
+        var yCoord = 0
+        var bitmapsSizes = 0
+
+        for (x in 0 until rows) {
+            var xCoord = 0
+            for (y in 0 until cols) {
+                var bitmapChunk: Bitmap? = Bitmap.createBitmap(bitmap, xCoord, yCoord, chunkWidth, chunkHeight)
+                val bitmapArray = getBytesFromBitmapChunk(bitmapChunk!!)
+                System.arraycopy(bitmapArray, 0, imageBytes, bitmapsSizes, bitmapArray.size)
+                bitmapsSizes = bitmapsSizes + bitmapArray.size
+                xCoord += chunkWidth
+
+                bitmapChunk.recycle()
+                bitmapChunk = null
+            }
+            yCoord += chunkHeight
+        }
+
+        return imageBytes
+    }
+
+    private fun getBytesFromBitmapChunk(bitmap: Bitmap): ByteArray {
+        val bitmapSize = bitmap.rowBytes * bitmap.height
+        val byteBuffer = ByteBuffer.allocate(bitmapSize)
+        bitmap.copyPixelsToBuffer(byteBuffer)
+        byteBuffer.rewind()
+        return byteBuffer.array()
+    }
+
     fun print(printables: ArrayList<Printable>) {
         this.printables = printables
         printingCallback?.connectingWithPrinter()
         bluetooth.connectToAddress(pairedPrinter.address)
     }
-
 }
